5025e32733d7a57024be9681be4feee67f560b22
Tyslenko Sergey <tyslenko.jobs@gmail.com>
2022-12-23 10:24:23 +0500

    add file 06.js (freecodecamp)

---
+++ Freecodecamp/JS_Algorithms_and_Data_Structures/06.js
@@ -0,0 +1,11 @@
+/**
+ * Freecodecamp #6
+ * variable
+ * 
+ * Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, 
+ * respectively.
+ */
+
+
+var myFirstName = "Sergey";
+var myLastName = "Tyslenko";
