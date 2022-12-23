dbba31325e59ec28b1c97820f1a9ebf4a2ad1ffc
Tyslenko Sergey <tyslenko.jobs@gmail.com>
2022-12-23 10:45:21 +0500

    add file 08.js (freecodecamp)

---
+++ Freecodecamp/08.js
@@ -0,0 +1,18 @@
+/**
+ * Freecodecamp #8
+ * variable
+ * 
+ * IModify the existing declarations and assignments so their names use camelCase.
+ * Do not create any new variables.
+ */
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
